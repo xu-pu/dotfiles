fd39a88b973dd939bc84911fbb309fbca437c076
sheep9275 <sheep9275@gmail.com>
2013-11-27 21:44:51 +0800

    more dot files added

---
+++ helper.py
@@ -0,0 +1,15 @@
+#!/bin/env python
+
+HOME_CONFS = [
+    '.bashrc',
+    '.emacs',
+    '.tmux.conf',
+]
+
+ROOT_CONFS = [
+    '/etc/bash.bashrc',
+    '/etc/DIR_COLORS'
+]
+
+if __name__ == '__main__':
+    pass
